1be7e4c6e814abbfd96208078362796e6850568c
ilias-imoula <iliasimoula97@gmail.com>
2020-03-02 10:17:29 +0100

    Prémier commit

---
+++ It.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="style.css">
+   <link href="https://fonts.googleapis.com/css?family=La+Belle+Aurore&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+   <title>Document</title>
+</head>
+
+<body>
+   <header>
+      <nav class="container d-flex justify-content-between">
+
+         <!--LOGO-->
+         <img src="asset/image/lettre-I-3d.jpg" height="40px" width="40px">
+
+         <!--MENU-->
+         <div>
+            <ul id="menu">
+               <a href=""><i class="fas fa-home"></i></a>
+               <a href=""><i class="fas fa-user"></i></a>
+               <a href=""><i class="fas fa-cog"></i></a>
+               <a href=""><i class="fas fa-route"></i></a>
+               <a href=""><i class="fas fa-project-diagram"></i></a>
+               <a href=""><i class="fas fa-envelope"></i></a>
+            </ul>
+         </div>
+
+         <!--SOCIAL-MEDIA-->
+         <div class="d-flex justify-content-end">
+            <ul id="menu" >
+               <a href="#"><i class="fab fa-linkedin-in"></i></a>
+               <a href="#"><i class="fab fa-twitter"></i></a>               
+                  <a href="-it" class="dropdown-toggle dropdown" data-toggle="dropdown" role="button"><img src="asset/image/flag_italy.svg" height="30px" width="30px"></a>
+                  <ul class="dropdown-menu">
+                     <a href="/portfolioILIAS"><img src="asset/image/flag_france.svg" height="30px" width="30px">  Fr</a></br>
+                     <a href="Eng.php"><img src="asset/image/flag_eng.svg" height="30px" width="30px">  Eng</a>
+                  </ul>                 
+               
+
+            </ul>
+         </div>
+         <div class="menu_burgher">
+            <div class="div_burgher"></div>
+            <div class="div_burgher"></div>
+            <div class="div_burgher"></div>
+         </div>
+      </nav>
+
+   </header>
+   <section>
+    
+   </section>
+   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+   <script src="https://kit.fontawesome.com/c96e1b0251.js" crossorigin="anonymous"></script>
+   <script src="app.js"></script>
+</body>
+
+</html>
+</body>
+</html>
